38f06ada9aa4f86163448f764b41140e4096d70b
Barzilay <jonatan2001@gmail.com>
2020-11-23 11:36:01 +0200

    Added some more advanced features

--- src/components/Troll.js
+++ src/components/Troll.js
@@ -2,10 +2,42 @@ import React from 'react';
 import trollface from '../trollface.png';
 import './Troll.sass'
 
-function Troll() {
-    return (
-        <img src={trollface} />
-    )
+
+class Troll extends React.Component {
+    constructor(props) {
+        super(props);
+        this.selfRef = React.createRef();
+
+        this.state = {visible: false};
+    }
+
+    handleClick = () => {
+        this.selfRef.current.style.left = "150px";
+    }
+
+    handleScroll = e => {
+        const wy = window.scrollY;
+        const iy = this.selfRef.current.offsetTop - window.innerHeight;
+        if (wy > iy + 50) {
+            this.setState({visible: true});
+        } else {
+            this.setState({visible: false});
+        }
+    }
+
+    componentDidMount = () => {
+        window.addEventListener("scroll", this.handleScroll);
+    }
+
+    componentWillUnmount = () => {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
+
+    render() {
+        return (
+            <img onClick={this.handleClick} src={trollface} ref={this.selfRef} className={this.state.visible ? "active img" : "img"} />
+        )
+    }
 }
 
 export default Troll;
